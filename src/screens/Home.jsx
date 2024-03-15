9b0216133be9eeaefffd4c657ba544cdfb9c6cfd
mearvinddevda <arvinddevda02@gmail.com>
2024-03-15 11:15:34 +0530

    Navbar Completed

---
+++ src/screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <h2>Home</h2>
+        <Footer/>
+    </div>
+  )
+}
